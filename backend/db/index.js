871406c18cb80276dc698ab3eb7f38c5f845a1f1
HashimPario <hashimpario@gmail.com>
2024-07-10 22:06:57 +0500

    deploy issue resolve

--- backend/db/index.js
+++ backend/db/index.js
@@ -7,7 +7,7 @@ const connectDb = () => {
   .connect(
     process.env.MONGO_URI ||
     "mongodb+srv://hashimpario:Hashim123@cluster0.goaqtkk.mongodb.net/parkingapp?retryWrites=true&w=majority&appName=Cluster0",
-      // "mongodb+srv://hashimpario:mpMzZplvAve2zCiD@cluster0.goaqtkk.mongodb.net/",
+      
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-//Routes
 app.use('/', require('./routes/userRouter'))
 
-// Database Connection
 connectDb();
 
-// app.use('/',(req,res,next)=>{
-//     res.json({msg: "Hello World"})
-//     console.log("hello")
-// })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
